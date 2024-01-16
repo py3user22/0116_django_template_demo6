3fe06a3bf2128409456421052e24658854f77ddd
JesusG <py3user22@github.com>
2024-01-15 20:19:39 -0600

    0115 add urls.py and html pages

--- app0115/urls.py
+++ app0115/urls.py
@@ -4,4 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('home', views.home),
+    path('0115_homepage_links.html', views.home),
+
+    path('git_notes', views.git_install),
+    path('0115_git_install_notes.html', views.git_install),
+
+    path('token_notes', views.token_home),
+    path('0115_django_token_auth_demo1.html', views.token_home),
+
+    path('django_debug', views.debug_install),
+    path('0115_django_debug_install_demo2.html', views.debug_install),
 ]
--- app0115/views.py
+++ app0115/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, '0115_homepage_links.html', {})
+
+def token_home(request):
+    return render(request, '0115_django_token_auth_demo1.html', {})
+def debug_install(request):
+    return render(request, '0115_django_debug_install_demo2.html', {})
+def git_install(request):
+    return render(request, '0115_git_install_notes.html', {})
